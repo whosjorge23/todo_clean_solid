8526d7794244947f1f5b58e3f12ef0745c79c96d
Giorgio Giannotta <jorge_upa@msn.com>
2023-10-29 15:47:56 +0100

    add quicksand fonts

---
+++ lib/utils/text_style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+TextStyle getQuicksandRegular() {
+  return const TextStyle(
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w400,
+  );
+}
+
+TextStyle getQuicksandBold() {
+  return const TextStyle(
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle getQuicksandSemiBold() {
+  return const TextStyle(
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w600,
+  );
+}
+
+TextStyle getQuicksandMedium() {
+  return const TextStyle(
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w500,
+  );
+}
+
+TextStyle getQuicksandLight() {
+  return const TextStyle(
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w300,
+  );
+}
