6e097afcf500b5bf2bd04ee01f71cd00ee6067e2
Giorgio Giannotta <jorge_upa@msn.com>
2023-11-11 10:20:59 +0100

    Add singleton for appColors

--- lib/shared_export.dart
+++ lib/shared_export.dart
@@ -1,7 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:todo_clean_solid/extension/quicksand_text_style.dart';
 import 'package:todo_clean_solid/services/shared_preferences_service.dart';
+import 'package:todo_clean_solid/theme/app_colors.dart';
 
 SharedPreferenceService get sharedPrefsService => GetIt.I.get<SharedPreferenceService>();
 
 QuicksandTextStyle get quickSandTextStyle => GetIt.I.get<QuicksandTextStyle>();
+
+AppColors get appColors => GetIt.I.get<AppColors>();
